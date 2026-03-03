b2f1957f8645f1eebff1b3a7b0658ebd3f8debca
Rafael Ferreira <rafa_fcontato@hotmail.com>
2026-03-03 20:51:02 -0300

    estudo de tipos de dados

--- 0202 Tipos de Dados/script.js
+++ 0202 Tipos de Dados/script.js
@@ -1,3 +1,54 @@
 var nome = 'rafael';
+var idade = 3;
+var time;
+var simbolo = Symbol();
+console.log(typeof nome);
 
-console.log(nome);
+var nome = 'grazi';
+var sobrenome = 'ferreira';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+var ano = '2018';
+var mes = 8;
+console.log(ano + mes);
+
+var frase1 = 'esse é o \"melhor\" jogo';
+
+console.log('funciona');
+
+var gols = 1000;
+var frase2 = `romario fez ${gols} gols`;
+
+console.log(frase2);
+
+
+// Declare uma variável contendo uma string
+var flor = 'rosa';
+
+// Declare uma variável contendo um número dentro de uma string
+var carros = '3';
+
+// Declare uma variável com a sua idade
+var minhaIdade = 3;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var meuNome = 'rafael';
+var sobreNome = 'Ferreira';
+var nomeCompleto = `${meuNome} ${sobreNome}`;
+
+console.log(nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+var time = `it's ime`;
+console.log(time);
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof meuNome);
+
+//ou
+
+var verificarIdade = typeof minhaIdade;
+
+console.log(verificarIdade);

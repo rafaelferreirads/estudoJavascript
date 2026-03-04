689f2a6f6befb3d9ba23f3c961b4b4fe1d1b8b90
Rafael Ferreira <rafa_fcontato@hotmail.com>
2026-03-03 21:09:38 -0300

    conserto de envios de pastas

---
+++ 0201_Variaveis/script.js
@@ -0,0 +1,56 @@
+console.log('funciona')
+
+var nome = 'Rafael';
+var idade = 3;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 30;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'rafael', 
+    cidade = 'minas';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+
+console.log(semDefinir);
+
+//console.log(aindaNãoDefini);
+
+//undefined
+var comida;
+console.log(comida);
+comida = 'bolo';
+console.log(comida);
+
+
+// var time = 'palmeiras';
+// time = 'bahia';
+
+// console.log(time);
+
+// Declarar uma variável com o seu nome
+var nome = 'Grazi'
+
+// Declarar uma variável com a sua idade
+var idade = 3;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+
+comidaFavorita = 'cachorro quente';
+
+// Declarar 5 variáveis diferentes sem valores
+
+var carro, aviao, cidade, floresta, cores;
+
+console.log(nome, idade, comidaFavorita, carro, aviao, cidade, floresta, cores)

cb1f81137de065becceb4938d5b582bb4bbc83ea
Rafael Ferreira <rafa_fcontato@hotmail.com>
2026-03-06 23:32:30 -0300

    finalizado segunda parte de objetos dois

--- 0206_Objetos_1/script.js
+++ 0206_Objetos_1/script.js
@@ -39,15 +39,48 @@ console.log(Math.random(), Math.PI);
 // Crie um objeto com os seus dados pessoais
 // Deve possui pelo menos duas propriedades nome e sobrenome
 
+const dados = {
+    nome: 'rafael',
+    sobrenome: 'ferreira',
+}
+
 // Crie um método no objeto anterior, que mostre o seu nome completo
+dados.nomeCompleto = function(){
+    return `${this.nome} ${this.sobrenome}`;
+}
+
+console.log(dados.nomeCompleto());
 
 // Modifique o valor da propriedade preco para 3000
 var carro = {
   preco: 1000,
   portas: 4,
   marca: 'Audi',
+ valor(){
+    return this.preco * 3;
+ },
 }
+
+//ou
+
+carro.preco = 3000;
+
+console.log(carro.valor(), carro.preco);
 
 // Crie um objeto de um cachorro que represente um labrador,
 // preto com 10 anos, que late ao ver um homem
 
+var cachorro = {
+    raca: 'preto',
+    cor: 'preto',
+    idade: 10,
+    latir(pessoa ='mulher'){
+        if (pessoa === 'homem'){
+            return 'latir';
+        } else {
+           return 'mudo';
+        }
+    }
+}
+
+console.log(cachorro.latir('homem'));

28b007e7fb96bafd6eebbad0326d1e4e10abdf8f
Rafael Ferreira <rafa_fcontato@hotmail.com>
2026-03-07 20:57:12 -0300

    primeira parte de arrays e loops fnalizado

---
+++ 0208_Arrays_e_Loops_1/script.js
@@ -0,0 +1,25 @@
+var videoGames = ['switch', 'ps4', 'xbox', 'nintendo'];
+
+console.log(videoGames[3]);
+//console.log(videoGames.pop(), videoGames.push['sega'], videoGames.length);
+for(let n = 0; n < 10; n++){
+    console.log(n);
+}
+
+var i =0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.

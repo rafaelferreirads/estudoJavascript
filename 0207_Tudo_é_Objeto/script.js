4afe8d8dbc0c8ab20f1defe37e2e6b2176bc68f5
Rafael Ferreira <rafa_fcontato@hotmail.com>
2026-03-07 18:03:10 -0300

    parte tudo e objetos finalizado

---
+++ 0207_Tudo_é_Objeto/script.js
@@ -0,0 +1,42 @@
+var nome = 'Rafael';
+
+nome.length;
+nome.toLowerCase();
+
+console.log(nome.length, nome.toLowerCase());
+
+var altura = 11.8;
+
+console.log(altura.toFixed(), altura.toString());
+
+var btn = document.querySelector('.btn');
+var divs = document.querySelector('.divs');
+var inpu = document.querySelector('.inputs');
+
+function mostrar(){
+
+}
+
+btn.addEventListener('click', function(){
+    mostrar();
+     console.log('clicou no btn');
+})
+
+
+// nomeie 3 propriedades ou métodos de strings
+
+carros = 'fuscA';
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+var btn = document.querySelector('.btn');
+btn.addEventListener;
+btn.appendChild;
+btn.id;
+btn.innerHTML;
+btn.outerHTML;
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+

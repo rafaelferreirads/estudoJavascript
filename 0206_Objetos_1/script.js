49b3f7663bd60fb00326d8ec4d18417fb73143dc
Rafael Ferreira <rafa_fcontato@hotmail.com>
2026-03-06 22:48:38 -0300

    objetos parte um finalizado

---
+++ 0206_Objetos_1/script.js
@@ -0,0 +1,53 @@
+var pessoa = {
+    nome: 'rafael',
+    idade: 3,
+    profissao: 'programador',
+    possuiFaculdade: true,
+}
+
+pessoa.nome;
+pessoa.profissao;
+
+console.log(pessoa.nome, pessoa.profissao);
+
+var quadrado = {
+    lados: 4,
+
+    area: function(lado){
+        return lado * lado;
+    },
+    perimetro: function(lado){
+        return this.lados * lado;
+    },
+
+    //ou
+
+    area2(lado){
+        return lado * lado;
+    },
+    perimetro2(lado){
+        return this.lados * lado;
+    },
+}
+
+console.log(quadrado.lados, quadrado.area(4), quadrado.perimetro(5), quadrado.area2(5));
+
+console.log(Math.random(), Math.PI);
+
+
+
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+

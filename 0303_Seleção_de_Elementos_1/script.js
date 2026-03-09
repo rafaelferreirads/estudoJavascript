b186ab49084f2a357ad55f189a5d3f41a8d4cb2c
Rafael Ferreira <rafa_fcontato@hotmail.com>
2026-03-09 19:28:08 -0300

    primeira parte de selecao de elementos finalizada

---
+++ 0303_Seleção_de_Elementos_1/script.js
@@ -0,0 +1,19 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const grid = document.getElementsByClassName('grid-section');
+console.log(grid.length);
+console.log(grid[1]);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+const linksInternos = document.querySelector('[href^="#"]');
+console.log(linksInternos);
+
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg);

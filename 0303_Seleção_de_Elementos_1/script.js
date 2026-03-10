5f51c9cf147d6d6786678a4ed46e4ebbd6c75e54
Rafael Ferreira <rafa_fcontato@hotmail.com>
2026-03-10 19:55:57 -0300

    etapa de selecao finalizada e inicio de exercicios

--- 0303_Seleção_de_Elementos_1/script.js
+++ 0303_Seleção_de_Elementos_1/script.js
@@ -17,3 +17,39 @@ console.log(linksInternos);
 
 const animaisImg = document.querySelectorAll('.animais img');
 console.log(animaisImg);
+
+const gridSectionHtml = document.getElementsByClassName('grid-section'); //aovivo
+const gridSectionNome = document.querySelectorAll('.grid-section');
+
+// primeiraUl.classList.add('grid-section');
+
+console.log(gridSectionHtml);
+console.log(gridSectionNome);
+
+gridSectionNome.forEach(function (item, index){
+    console.log(item);
+});
+
+const arrayGrid = Array.from(gridSectionHtml);
+
+arrayGrid.forEach(function (item){
+    console.log(item);
+})
+
+
+// Retorne no console todas as imagens do site
+
+const imagens = document.querySelectorAll('img');
+console.log(imagens);
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+
+const imagens2 = document.querySelectorAll('[href^="imagem"]');
+
+console.log(imagens2);
+// Selecione todos os links internos (onde o href começa com #)
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+
+// Selecione o último p do site
+

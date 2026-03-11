f3f626130a1f72ee1ff567356c80b71e86815a59
Rafael Ferreira <rafa_fcontato@hotmail.com>
2026-03-10 22:33:21 -0300

    etapa de selecao de elementos com exercicios finalizada

--- 0303_Seleção_de_Elementos_1/script.js
+++ 0303_Seleção_de_Elementos_1/script.js
@@ -44,12 +44,28 @@ console.log(imagens);
 
 // Retorne no console apenas as imagens que começaram com a palavra imagem
 
-const imagens2 = document.querySelectorAll('[href^="imagem"]');
+const imagens2 = document.querySelectorAll('img[src^="img/imagem"]');
 
 console.log(imagens2);
+
 // Selecione todos os links internos (onde o href começa com #)
+
+const links = document.querySelectorAll('[href^="#"]');
+
+console.log(links)
 
 // Selecione o primeiro h2 dentro de .animais-descricao
 
+const titulo2 = document.querySelector('.animais-descricao h2');
+
+console.log(titulo2);
+
 // Selecione o último p do site
+
+const paragrafo = document.querySelectorAll('p');
+const paragrafo2 = document.querySelectorAll('p');
+
+console.log(paragrafo[paragrafo.length - 1]);
+//ou
+console.log(paragrafo[--paragrafo.length]);
 

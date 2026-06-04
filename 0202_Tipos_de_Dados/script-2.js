5f7c5627727c39333f79a297568e46391bbc516d
Rafael Ferreira <rafa_fcontato@hotmail.com>
2026-06-04 14:58:59 -0300

    tipos de dados finalizado

---
+++ 0202_Tipos_de_Dados/script-2.js
@@ -0,0 +1,34 @@
+// Declare uma variável contendo uma string
+
+let valorNome = 'Rafael';
+
+// Declare uma variável contendo um número dentro de uma string
+
+let valorNumero = '30';
+
+// Declare uma variável com a sua idade
+
+let valorIdade = 39;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+let novoNome = 'Rafael';
+let novoSobrenome = "ferreira";
+
+console.log(`${novoNome} ${novoSobrenome}`);
+
+// Coloque a seguinte frase em uma variável: It's time
+
+let frase = `It's time`
+
+console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+ console.log(typeof novoNome);
+
+ //ou
+
+ verificaNome = typeof novoNome;
+
+ console.log(verificaNome);

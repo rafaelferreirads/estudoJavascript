555034a2e544edd7133b957f5f524ef547184639
Rafael Ferreira <rafa_fcontato@hotmail.com>
2026-06-04 14:12:51 -0300

    nova atualização de curso

---
+++ 0201_Variaveis/script-2.js
@@ -0,0 +1,11 @@
+// Declarar uma variável com o seu nome
+let nome = 'rafael';
+// Declarar uma variável com a sua idade
+let idade = 39;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let churrasco;
+// Atribuir valor a sua comida favorita
+churrasco = 'maminha';   
+// Declarar 5 variáveis diferentes sem valores
+let, carro, frutas, pais, informatica, papelaria;

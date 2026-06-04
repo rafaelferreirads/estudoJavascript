1dc9823017ac4066a5132117c468d2f50ecaf030
Rafael Ferreira <rafa_fcontato@hotmail.com>
2026-06-04 15:26:36 -0300

    numeros e operadores finalizados

---
+++ 0203_Números_e_Operadores_1/script-2.js
@@ -0,0 +1,45 @@
+let idade = 39;
+console.log(idade);
+
+let soma1 = 10 + 10 + 20 + 30 * 4 / 2 + 10;
+console.log(soma1);
+
+let incremento = 1;
+console.log(++incremento);
+
+let novaIdade = '28';
+let somaIdade = 2;
+
+console.log(+novaIdade + somaIdade);
+
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; //35
+
+// Crie duas expressões que retornem NaN
+
+let naoEnumero = 'numero 10' / 2;
+let naoEnumero2 = 'numero 10' - 2;
+
+// Somar a string '200' com o número 50 e retornar 250
+
+let duzentos = '200';
+let cinquenta = 50;
+
+console.log(+duzentos + 50);
+
+//ou
+
+let soma = +'200' + 50;
+console.log(soma);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+let novoIncremento = 5;
+console.log(++novoIncremento);
+
+// Como dividir o peso por 2?
+var numero = +'80' /2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
+console.log(peso);

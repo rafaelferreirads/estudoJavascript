25d96a8582ee41b09860079a10c1bf6134d58da5
Rafael Ferreira <rafa_fcontato@hotmail.com>
2026-06-06 14:09:49 -0300

    parde de tudo é onj finalizado

---
+++ 0207_Tudo_é_Objeto/script-2.js
@@ -0,0 +1,19 @@
+let nome = 'Rafael';
+
+console.log(nome.charAt(5));
+console.log(nome.toUpperCase());
+
+console.log(nome.length);
+
+
+// nomeie 3 propriedades ou métodos de strings
+
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+let btn = document.querySelector('.btn');
+
+console.log(btn.ariaColIndex);
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
